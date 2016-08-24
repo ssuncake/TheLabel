221bcffc9a0533622cf143bff5d02834f05b672e
Lee JungHo <blue0626@naver.com>
2016-08-24 14:52:09 +0900

    레이블 프레그먼트 연결 완료 정말

---
+++ app/src/main/java/team/nuga/thelabel/Fragment/LabelContainerFragment.java
@@ -0,0 +1,49 @@
+package team.nuga.thelabel.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import team.nuga.thelabel.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LabelContainerFragment extends Fragment {
+
+
+    public LabelContainerFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_label_container, container, false);
+        getChildFragmentManager().beginTransaction().replace(R.id.frameLayout_LabelContainer,new LabelSelectFragment()).commit();
+        return view;
+    }
+
+    public void selectLabel(String labelName){
+
+        Bundle bundle = new Bundle();
+        bundle.putString("LabelName",labelName);
+        LabelMainFragment selectedLabelFragment = new LabelMainFragment();
+        selectedLabelFragment.setArguments(bundle);
+
+
+        getChildFragmentManager().beginTransaction().replace(R.id.frameLayout_LabelContainer,selectedLabelFragment).commit();
+    }
+
+
+    public void makeLabel(){
+        getChildFragmentManager().beginTransaction().replace(R.id.frameLayout_LabelContainer,new LabelMakeFragment()).commit();
+    }
+
+}
--- app/src/main/java/team/nuga/thelabel/Fragment/LabelMainFragment.java
+++ app/src/main/java/team/nuga/thelabel/Fragment/LabelMainFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import team.nuga.thelabel.R;
 
 /**
@@ -31,14 +32,17 @@ public class LabelMainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_label_main, container, false);
-
+        ButterKnife.bind(this,view);
+        labelSetting();
 
         return view;
     }
 
 
+
+
     public void labelSetting(){
-        labelName = getArguments().getString("labelName"); // 메인액티비티로 부터 Bundle로 전달받은 레이블 이름을 Label네임에 저장
+        labelName = getArguments().getString("LabelName"); // 메인액티비티로 부터 Bundle로 전달받은 레이블 이름을 Label네임에 저장
         labelNameView.setText(labelName);
     }
 
