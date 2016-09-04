485197b7ed0ae2518e88e80bb2f43650c03ca38b
JungHo Lee <blue0626@naver.com>
2016-09-04 19:41:13 +0900

    설정 ui

--- app/src/main/java/team/nuga/thelabel/fragment/SettingFragment.java
+++ app/src/main/java/team/nuga/thelabel/fragment/SettingFragment.java
@@ -1,33 +1,24 @@
 package team.nuga.thelabel.fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import team.nuga.thelabel.PasswordSettingActivity;
-import team.nuga.thelabel.PushAlramSettingActivity;
 import team.nuga.thelabel.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingFragment extends Fragment {
-    @BindView(R.id.button_myimpormaton)
-    Button impormationbutton;
-    @BindView(R.id.button_alram_setting)
-    Button alrambutton;
 
     public SettingFragment() {
         // Required empty public constructor
     }
+
 
 
     @Override
@@ -38,17 +29,6 @@ public class SettingFragment extends Fragment {
         ButterKnife.bind(this, view);
         return view;
     }
-    @OnClick(R.id.button_myimpormaton)
-    public void impormationClick(View v){
-        Intent intent = new Intent(getActivity(), PasswordSettingActivity.class);
-        startActivity(intent);
 
-    }
-
-    @OnClick(R.id.button_alram_setting)
-    public void alramClick(View v){
-        Intent intent = new Intent(getActivity(), PushAlramSettingActivity.class);
-        startActivity(intent);
-    }
 
 }
