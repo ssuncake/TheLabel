3d7a9269e9fb57fb15520732104d3e088ce38e33
Lee JungHo <blue0626@naver.com>
2016-09-13 10:03:10 +0900

    내가 좋아하는 게시물

--- app/src/main/java/team/nuga/thelabel/fragment/UploadMusicFragment.java
+++ app/src/main/java/team/nuga/thelabel/fragment/UploadMusicFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,7 +18,7 @@ import team.nuga.thelabel.R;
 public class UploadMusicFragment extends Fragment {
 
    @BindView(R.id.textView_MusicUpload_FilePath)
-
+    TextView filePath;
 
     public UploadMusicFragment() {
         // Required empty public constructor
